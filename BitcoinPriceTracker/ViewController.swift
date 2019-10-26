90d303e54bdd69b09bf13a5277bb8333ab077618
MAwais <mawais54013@outlook.com>
2019-10-25 22:41:11 -0700

    updated

--- BitcoinPriceTracker/ViewController.swift
+++ BitcoinPriceTracker/ViewController.swift
@@ -13,6 +13,28 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        if let url = URL(string: "https://min-api.cryptocompare.com/data/price?fsym=BTC&tsyms=USD,JPY,EUR") {
+            URLSession.shared.dataTask(with: url) { (data: Data?, response:URLResponse?, error:Error?) in
+                if error == nil {
+                    if data != nil {
+                        if let json = try? JSONSerialization.jsonObject(with: data!, options: []) as?
+                            [String:Double] {
+                            if let usdPrice = json["USD"] {
+                                print(usdPrice)
+                            }
+                            if let eurPrice = json["EUR"] {
+                                print(usdPrice)
+                            }
+                        }
+                    }
+                }
+                else
+                {
+                    print("we have a problem")
+                }
+            }.resume()
+        }
     }
 
 
